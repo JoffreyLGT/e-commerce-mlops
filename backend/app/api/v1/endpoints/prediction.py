28d8c81804dfde0de84ec1e5d6ec206626539207
Landry-G01 <128144604+Landry-G01@users.noreply.github.com>
2023-07-27 16:50:40 +0000

    Add limit parameter to /predict
    - remove png format from images

--- backend/app/api/v1/endpoints/prediction.py
+++ backend/app/api/v1/endpoints/prediction.py
@@ -16,10 +16,10 @@ router = APIRouter()
 @router.post("/", response_model=List[schemas.PredictionResult])
 async def predict_category(
     *,
-    _db: Session = Depends(deps.get_db),
     designation: str = "",
     description: str = "",
     image: UploadFile | None = None,
+    limit: int | None = None,
     _current_user: models.User = Depends(deps.get_current_active_user),
 ) -> Any:
     """
@@ -28,15 +28,24 @@ async def predict_category(
     if image is None:
         image_data = np.zeros((254, 254, 3))
     else:
-        extension = image.filename.split(".")[-1] in ("jpg", "jpeg", "png")
+        extension = image.filename.split(".")[-1] in ("jpg", "jpeg")
         if extension is False:
             raise HTTPException(
                 400, detail="Invalid image extension. Must be jpg or jpeg."
             )
         image_data = Image.open(BytesIO(await image.read()))
 
+    if limit is not None and limit <= 0:
+        raise HTTPException(
+            400,
+            detail="Invalid value for limit. Must be a positive integer.",
+        )
+
     result = get_prediction(
-        str(designation or ""), str(description or ""), np.asarray(image_data)
+        designation,
+        description,
+        np.asarray(image_data),
+        limit,
     )
     return [
         schemas.PredictionResult(prdtypecode=i[0], probabilities=i[1], label=i[2])
