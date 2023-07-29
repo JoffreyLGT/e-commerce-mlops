a2af2bc2d899ba22bfd23aa0529c1cb7900d0298
Joffrey Lagut <j.lagut@live.fr>
2023-07-29 16:36:58 +0200

    Add telemetry to API
    - OpenTelemetry is used with automated telemetry
    - Update requirements.txt to add telemetry libs
    - Update README.md to change the command line to start the server
    - We are now starting the server and making sure it's available to all

--- backend/start-reload.sh
+++ backend/start-reload.sh
@@ -1,6 +1,12 @@
 #! /usr/bin/env sh
 
+# Script used to start the API with --reload option.
+# Made to facilitate development.
+# Warning: telemetry is not activated, no event will be sent to SigNoz.
+
+
 # Run prestart.sh to create DB
 ./prestart.sh
 
-exec uvicorn app.main:app --reload
+echo "Start with reload"
+exec uvicorn app.main:app --reload --host 0.0.0.0 --port 8000
