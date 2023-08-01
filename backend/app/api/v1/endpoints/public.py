46a2b4a2757375e14de4ed0c4acbf9151f4bd021
Joffrey Lagut <j.lagut@live.fr>
2023-08-01 20:53:31 +0000

    Add db dependency in is_online
    - The goal of the function being to ensure the API is up and running,
    we must check DB connection.

--- backend/app/api/v1/endpoints/public.py
+++ backend/app/api/v1/endpoints/public.py
@@ -1,10 +1,24 @@
-from fastapi import APIRouter
+"""Contains a public route to check if the API is online."""
 
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from app.api import dependencies
 from app.core.settings import settings
-
 
 router = APIRouter()
 
+
 @router.get("/")
-async def is_online():
+async def is_online(
+    _db: Session = Depends(dependencies.get_db),
+):
+    """Check if the API is running and if DB session is active.
+
+    Args:
+        _db: active db session.
+
+    Returns:
+        Message saying the API is online.
+    """
     return {f"API {settings.PROJECT_NAME} is online!"}
