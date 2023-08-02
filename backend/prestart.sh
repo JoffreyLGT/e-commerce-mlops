50630fe85bb5c56d7c417c54060ac93f5614e246
Joffrey Lagut <j.lagut@live.fr>
2023-08-02 18:53:28 +0200

    Move backend/app/scripts to backend/scripts and add test scripts
    - Add lint.sh to run formatter and linters in checkonly mode to
    report issues.
    - Add test.sh deploy docker environment and start lint.sh and
    tests-start.sh before cleaning up.
    - Update all path to match the new scripts folder.
    - Add missing modules in requirements-dev.txt.
    - Update backend.Dockerfile to add test variant.

--- backend/prestart.sh
+++ backend/prestart.sh
@@ -1,13 +1,13 @@
 #! /usr/bin/env bash
 
 # Let the DB start
-python app/scripts/backend_pre_start.py
+python scripts/backend_pre_start.py
 
 # Run migrations
 alembic upgrade head
 
 # Create initial data in DB
-python app/scripts/seed_data.py
+python scripts/seed_data.py
 
 # Download model saves
-python app/scripts/download_models.py
+python scripts/download_models.py
