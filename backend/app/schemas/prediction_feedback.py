9a8d5abe8f27a43a7ee84e1310edd72b17491901
Joffrey Lagut <j.lagut@live.fr>
2023-08-02 10:00:39 +0000

    Update prediction_feedback schema
    - Add functions to serialize to json and deserialize a json object
    - Fix docstrings
    - Move validator before child class

--- backend/app/schemas/prediction_feedback.py
+++ backend/app/schemas/prediction_feedback.py
@@ -1,7 +1,7 @@
-"""
-Define Pydantic schemas for a PredictionFeedback object.
-"""
+"""Define Pydantic schemas for a PredictionFeedback object."""
 
+
+import json
 
 from pydantic import BaseModel, validator
 
@@ -9,20 +9,45 @@ from app.core.security import settings
 
 
 class PredictionFeedbackBase(BaseModel):
-    """
-    Shared attributes between each class.
-    """
+    """Shared attributes between each class."""
 
     product_id: int
     model_version: str | None = "1.0"
     real_category_id: int
     pred_category_id: int
 
+    def to_json_str(self) -> str:
+        """Export object to a string with JSON format.
+
+        Returns:
+            Object in a string with JSON format.
+        """
+        return json.dumps(self.__dict__)
+
+    def to_json(self):
+        """Export object to JSON format.
+
+        Returns:
+            Object in JSON format.
+        """
+        return json.loads(self.to_json_str())
+
+    @classmethod
+    def from_json(cls, json_str):
+        """Load object from a JSON.
+
+        Args:
+            json_str: JSON of the object.
+
+        Returns:
+            A PredictionFeedbackInDBBase with the information from json_str.
+        """
+        json_dict = json.loads(json_str)
+        return cls(**json_dict)
+
 
 class PredictionFeedbackCreate(PredictionFeedbackBase):
-    """
-    Properties to receive via API on creation.
-    """
+    """Properties to receive via API on creation."""
 
     product_id: int
     real_category_id: int
@@ -30,21 +55,22 @@ class PredictionFeedbackCreate(PredictionFeedbackBase):
 
 
 class PredictionFeedbackUpdate(PredictionFeedbackBase):
-    """
-    Properties to receive via API on update.
-    """
+    """Properties to receive via API on update."""
 
 
 class PredictionFeedbackInDBBase(PredictionFeedbackBase):
-    """
-    Base properties stored in DB. They are the one needed for all entries.
-    """
+    """Base properties stored in DB. They are the one needed for all entries."""
 
     id: int
     model_version: str | None = None
     product_id: int
     real_category_id: int
     pred_category_id: int
+
+    @validator("model_version")
+    def set_model_version(cls, model_version):  # pylint: disable=E0213
+        """Assign a default value to model_version if set to None."""
+        return model_version or settings.MODEL_VERSION
 
     class Config:  # pylint: disable=R0903
         """
@@ -58,19 +84,10 @@ class PredictionFeedbackInDBBase(PredictionFeedbackBase):
         orm_mode = True
         validate_assignment = True
 
-    @validator("model_version")
-    def set_model_version(cls, model_version):  # pylint: disable=E0213
-        """Assign a default value to model_version if set to None."""
-        return model_version or settings.MODEL_VERSION
-
 
 class PredictionFeedback(PredictionFeedbackInDBBase):
-    """
-    Properties to return to client. By default, all the base properties stored in DB.
-    """
+    """Properties to return to client. By default, all the base properties stored in DB."""
 
 
 class PredictionFeedbackInDB(PredictionFeedbackInDBBase):
-    """
-    Additional properties stored in DB. Most of the time, they are optionals.
-    """
+    """Additional properties stored in DB. Most of the time, they are optionals."""
