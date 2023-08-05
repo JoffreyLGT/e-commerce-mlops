95f40f7009f31efe13b4ad31c4bf55cdc6ed61ee
Joffrey Lagut <j.lagut@live.fr>
2023-08-05 13:06:57 +0000

    Fix an issue that could occur when sending images with wrong encoding
    - This case happens when you download a PNG, rename it into JPEG.

--- backend/app/api/v1/endpoints/prediction.py
+++ backend/app/api/v1/endpoints/prediction.py
@@ -51,6 +51,8 @@ async def predict_category(
         try:
             # Open the image with PIL
             image_data = np.asarray(Image.open(BytesIO(await image.read())))
+            if image_data.shape[2] > 3:
+                raise UnidentifiedImageError()
         except UnidentifiedImageError as exc:
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
