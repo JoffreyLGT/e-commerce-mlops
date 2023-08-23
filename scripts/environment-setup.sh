11df6528869ee2d2f48b2fcab454d06f206ed4a2
Joffrey Lagut <j.lagut@live.fr>
2023-08-23 10:43:32 +0200

    Add a script to start db container for local environment
    - Add IS_DEV_CONTAINER env variable in dev container to ensure we are not starting another DB container
    - Add USE_DB_CONTAINER env variable to allow the usage of local PostgreSQL.
    - Add missing settings in root project to hide backend and datascience dirs.
    - Add tasks to start and stop DB container.

--- scripts/environment-setup.sh
+++ scripts/environment-setup.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-
 PYTHON_EXE=python3.11
 # Exit if an error occurs
 set -e
@@ -16,21 +15,17 @@ root_dir="$(dirname "$current_dir")"
 # Get current user name
 user="$(id -un)"
 
-
 backend_venv=$root_dir/backend/.venv
 # Check if backend_venv exists and if RESET_VENV is true
-if [[ -d "$backend_venv" && "$RESET_VENV" == "true" ]]
-then
+if [[ -d "$backend_venv" && "$RESET_VENV" == "true" ]]; then
     echo "RESET_VENV is set to true, delete backend venv to recreate it."
     rm -rf $backend_venv
-elif [[ -d "$backend_venv" && ! -e "$backend_venv/$user" ]]
-then
+elif [[ -d "$backend_venv" && ! -e "$backend_venv/$user" ]]; then
     echo "backend venv exists but was created by another user, delete it."
     rm -rf $backend_venv
 fi
 # Create the venv only if it doesn't exists.
-if [ -d "$backend_venv" ]
-then
+if [ -d "$backend_venv" ]; then
     echo "Backend venv already exists."
 else
     echo "Setup backend venv."
@@ -43,24 +38,28 @@ fi
 echo "Setup datascience venv"
 datascience_venv=$root_dir/datascience/.venv
 # Check if datascience_venv exists and if RESET_VENV is true
-if [[ -d "$datascience_venv" && "$RESET_VENV" == "true" ]]
-then
+if [[ -d "$datascience_venv" && "$RESET_VENV" == "true" ]]; then
     echo "RESET_VENV is set to true, delete datascience venv to recreate it."
     rm -rf $datascience_venv
-elif [[ -d "$datascience_venv" && ! -e "$datascience_venv/$user" ]]
-then
+elif [[ -d "$datascience_venv" && ! -e "$datascience_venv/$user" ]]; then
     echo "datascience venv exists but was created by another user, delete it."
     rm -rf $datascience_venv
 fi
 
-if [ -d "$datascience_venv" ]
-then
+if [ -d "$datascience_venv" ]; then
     echo "datascience venv already exists."
 else
     cd $root_dir/datascience
     poetry install
     touch "$datascience_venv/$user"
     echo "Done"
+fi
+
+# Start DB container for local environment
+if [[ $IS_DEV_CONTAINER != "true" && $USE_DB_CONTAINER == "true" ]]; then
+    cd $root_dir
+    echo "Start db container"
+    TARGET=development docker-compose -f docker-compose.yaml up -d db
 fi
 
 echo "Environment successfully configured"
