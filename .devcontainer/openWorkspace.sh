11b92a65e98e38ff57c3aa8aec4d30563abb1274
Joffrey Lagut <j.lagut@live.fr>
2023-08-28 19:23:01 +0200

    Remove python.envFile settings to be on the same env during dev as when deployed

--- .devcontainer/openWorkspace.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-
-# Open workspace in current window when code is available on container
-until code -r e-commerce-mlops.code-workspace Welcome.md
-do
-    echo "Waiting for code to be installed on container."
-    sleep 2
-done
