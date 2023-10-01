473ceae375a9ec379394c97c5aadce0e5f87e70b
Joffrey Lagut <j.lagut@live.fr>
2023-10-01 10:05:56 +0200

    fix: fix backend tests and deployment
    We are now setting up the project using deployment script to start unit tests.
    Deployment is not more configurable than ever!
    Document the usage of environment files for configurations.
    Add multiple configuration files.
    
    Refs: #103, #145, $152

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,8 +1,7 @@
-from logging.config import fileConfig
 import os
+from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
+from sqlalchemy import engine_from_config, pool
 
 from alembic import context
 
@@ -16,6 +15,7 @@ if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
 from app.database.base import Base  # noqa
+
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
@@ -27,12 +27,14 @@ target_metadata = Base.metadata
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
 
+
 def get_url():
     user = os.getenv("POSTGRES_USER", "postgres")
     password = os.getenv("POSTGRES_PASSWORD", "")
-    server = os.getenv("POSTGRES_SERVER", "db")
+    server = os.getenv("DB_SERVER", "db")
     db = os.getenv("POSTGRES_DB", "app")
     return f"postgresql://{user}:{password}@{server}/{db}"
+
 
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
@@ -66,7 +68,6 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
-
     configuration = config.get_section(config.config_ini_section)
     configuration["sqlalchemy.url"] = get_url()
 
--- datascience/scripts/start.sh
+++ datascience/scripts/start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Start MLFlow UI"
-poetry run mlflow ui -h 0.0.0.0 -p 5000 \
+poetry run mlflow ui -h 0.0.0.0 -p $DATASCIENCE_MLFLOW_PORT \
 --backend-store-uri "$MLFLOW_REGISTRY_URI" \
 --default-artifact-root "$MLFLOW_TRACKING_URI" & \
 echo "Start fusion model serving" & \
-poetry run mlflow models serve -m "models:/fusion/Production" -h 0.0.0.0 -p 5002 --env-manager local
+poetry run mlflow models serve -m "models:/fusion/Production" -h 0.0.0.0 -p $DATASCIENCE_MODEL_PORT --env-manager local
 
 wait
