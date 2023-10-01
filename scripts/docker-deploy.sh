f78b020338a6eccf0f4cadde707c87a12815711b
Joffrey Lagut <j.lagut@live.fr>
2023-10-01 15:33:39 +0200

    ci: fix backend unit tests
    Refs: #155

--- scripts/docker-deploy.sh
+++ scripts/docker-deploy.sh
@@ -11,7 +11,7 @@ me=$(basename "$0")
 
 echo "$me - Setup datascience venv"
 cd datascience
-poetry install && poetry run python -m scripts.download_mlruns
+poetry install --only download && poetry run python -m scripts.download_mlruns
 cd -
 
 echo "$me - Using $2 as env file"
@@ -19,7 +19,7 @@ echo "$me - Using $2 as env file"
 network=$(grep "DOCKER_NETWORK=" $2 | sed -e 's/.*=//')
 echo "$me - Found DOCKER_NETWORK=$network"
 
-echo "$me - Create project containers and start them"
+echo "$me - Build images, create containers and start them detached"
 docker compose $1 $2 -f docker-compose.yaml up --build --remove-orphans -d
 
 if [[ "$TARGET_ENV" == "production" ]]; then
--- scripts/run-tests-in-containers.sh
+++ scripts/run-tests-in-containers.sh
@@ -10,5 +10,9 @@ set -e
 
 # Build and start containers
 scripts/docker-deploy.sh --env-file staging.env
+
+echo "Wait 5 seconds to give enough time for backend prestart script to be done"
+sleep 5
+
 # Execute start-tests script
 docker exec --workdir /backend api-staging scripts/start-tests.sh
